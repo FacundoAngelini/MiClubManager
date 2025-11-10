0eab1ca6af23507acc19d45e5d85c1d8784af362
Facundo Ariel Angelini <facundoangelini@gmail.com>
2025-11-10 14:35:23 -0300

    main

--- tp final/src/Main.java
+++ tp final/src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
+        double presupuesto_inicial =0;
+        Club club = new Club(presupuesto_inicial);
+        club.getGestionPresupuesto().agregar_fondos(500000, "Sponsor ropa", "1/2/2025");
+
     }
 
 }
