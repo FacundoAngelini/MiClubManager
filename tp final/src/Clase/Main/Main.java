5980d881d601debe9dc94328c7291d9a96fb67dc
Facundo Ariel Angelini <facundoangelini@gmail.com>
2025-11-13 18:06:46 -0300

    cambios

--- tp final/src/Clase/Main/Main.java
+++ tp final/src/Clase/Main/Main.java
@@ -13,12 +13,7 @@ public class Main {
         try {
             club.getGestionPresupuesto().agregar_fondos(3000000, "Sponsor ropa", "1/2/2025");
             club.getGestionEstadios().agregarEstadio("Libertad", 50000, "Buenos Aires", 100000);
-            club.getGestionSocios().agregarSocio(
-                    "65489321", "Ramiro", "Perez",
-                    "13/06/2019", "Argentina",
-                    true, "13/10/2021",
-                    Tiposocio.JUVENIL
-            );
+            club.getGestionSocios().agregarSocio("65489321", "Ramiro", "Perez", "13/06/2019", "Argentina", true, "13/10/2021", Tiposocio.JUVENIL);
         } catch (Exception e) {
             System.out.println("Error al agregar datos iniciales: " + e.getMessage());
         }
